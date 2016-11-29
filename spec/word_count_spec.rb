209b5403a4598b54127bfbb5f1f8364623562058
Eric Bomblatus <ebomblatus@gmail.com>
2016-11-28 22:40:24 -0800

    Update word_count spec for edgecase

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -8,10 +8,10 @@ describe ('String#word_count') do
   it('will match one word in a multiple word string') do
    expect('cat'.word_count('the cat chased the mouse')).to(eq(1))
   end
-  it('will count the number of occurances for a given word') do
-    expect('fish'.word_count('one fish two fish red fish blue fish')).to(eq(4))
+  it('will match one word in a multiple word string using mixed case ') do
+    expect('banana'.word_count('The Monkey ate a Banana and Felt Happy')).to(eq(1))
   end
-  # it('will count a word when punctuation is included') do
-  #   expect("evil".word_count("hear no evil, speak no evil, see no evil")).to(eq(3))
-  # end
+  it('will count the number of occurances for a given word') do
+    expect('fish'.word_count('One Fish Two Fish Red Fish Blue Fish')).to(eq(4))
+  end
 end
